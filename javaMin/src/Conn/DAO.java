dbec67ebe6a7277a50aaa83641307130bd7967ce
smhrd <smhrd@DESKTOP-4AKO3AF>
2023-05-10 16:43:43 +0900

    -- 그만 ㅠㅠ

--- javaMin/src/Conn/DAO.java
+++ javaMin/src/Conn/DAO.java
@@ -4,18 +4,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class DAO {
-	int count = 6;
-
-	public void time() { // 타이머
+	int countmain = 6;
+	int countfirst = 5;
+	public void timeMain() { // 타이머
 		Timer m = new Timer();
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
-				if (count > 0) {
-					System.out.println("[" + count + "0" + "초 남았습니다." + "]");
-					count--;
+				if (countmain > 0) {
+					System.out.println("[" + countmain + "0" + "초 남았습니다." + "]");
+					countmain--;
 				} else {
 					System.out.println("실패");
+					countmain = 6;
 					m.cancel();
 				}
 			}
@@ -23,4 +24,23 @@ public class DAO {
 		m.schedule(task, 10 * 100, 10000);
 	}//<-- time
 
+	public void TimeFirst() {
+		Timer m = new Timer();
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				if (countfirst > 0) {
+					System.out.println("[ " + countfirst + " ]");
+					countfirst--;
+				} else {
+					System.out.println("시작!");
+					countfirst = 5;
+					m.cancel();
+				}
+			}
+		};
+		m.schedule(task, 0, 1000);
+	}//<-- TimeFirst
+		
+	
 }
